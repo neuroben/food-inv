92f101f3f1a4bd02386de83a0f59717281112983
Bence A Kovacs <bencearon@gmail.com>
2024-02-03 17:21:19 +0000

    forget to push so i did the datatable sync and the login redirect also haha

--- client/food/script.js
+++ client/food/script.js
@@ -1,1 +1,30 @@
 console.log('food script loaded!');
+
+fetch('https://1337-neuroben-foodinv-inb7p9a3qw8.ws-eu107.gitpod.io/api/foods',{
+    method: "GET",
+    headers: {    
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzA2OTczMzA2LCJleHAiOjE3MDk1NjUzMDZ9.7VdBy_L7yfCxEgbhcJCEOuMuC8Zt8lx26j7FIH1w10A'
+    }
+})
+    .then(function (response){
+        return response.json();
+    })
+    .then(apiJsonData => apiJsonData.data)
+    .then(function (apiJsonData){
+        console.log(apiJsonData);
+        renderData(apiJsonData);
+    });
+
+function renderData(foods){
+    const table = document.getElementById("table");
+    const propertiesToInclude = ['food', 'quantity', 'unit', 'price', 'description']; // filter the important ones
+    foods.forEach(foodItem => {
+        let newRow = document.createElement("tr");
+        propertiesToInclude.forEach((property) => {
+            let cell = document.createElement("td");
+            cell.innerText = foodItem.attributes[property];
+            newRow.appendChild(cell);
+        })
+        table.appendChild(newRow);
+    });
+}
